312c53628fca05f650d5bcaac40f4a6a2f2dfdae
2884069 <2884069w@student.gla.ac.uk>
2025-03-05 15:53:08 +0000

    added basics of whats specified in our specification

--- inqpal/models.py
+++ inqpal/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    POST_MAX_LEN = 2000
+
+    # id?
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=POST_MAX_LEN)
+    image = models.ImageField(upload_to='post_images', blank=True)
+    # category = 
+    # number_roars = models.IntegerField(default=0)
+    # roar relation (M:N)
+
+    def __str__(self):
+        # should get some unique identifier
+        return self.id
+
+class Comment(models.Model):
+    COMMENT_MAX_LEN = 500
+
+    # id? if so based off Post id
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=COMMENT_MAX_LEN)
+
+    def __str__(self):
+        # should get some unique identifier
+        return self.id
+    
+# class Account(models.Model):
+# implementation varies depending on how we decide on doing it
+# currently above assume utilising built-in django user
--- inqpal/urls.py
+++ inqpal/urls.py
@@ -5,4 +5,14 @@ app_name = 'inqpal'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts/trending/', views.trending, name='trending'),
+    path('posts/pals/', views.palsposts, name='palsposts'),
+    path('posts/categories/', views.categories, name='categories'),
+    path('posts/categories/<slug:category_name_slug>/',views.show_category, name='show_category'),
+    path('account/signup/', views.signup, name='signup'),
+    path('account/login/', views.login, name='login'),
+    path('account/my_account/', views.my_account, name='my_account'),
+    path('account/my_account/make_post', views.make_post, name='make_post'),
+    path('account/my_account/edit_profile', views.edit_profile, name='edit_profile'),
+    path('account/my_account/add_pal', views.add_pal, name='add_pal'),
 ]
