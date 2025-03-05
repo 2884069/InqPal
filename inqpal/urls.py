73336a05e0d53a6697962b55677d3c2b187719b7
“Amber <2909415o@student.gla.ac.uk>
2025-03-05 16:25:28 +0000

    added delegations

--- inqpal/urls.py
+++ inqpal/urls.py
@@ -4,15 +4,15 @@ from inqpal import views
 app_name = 'inqpal'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/trending/', views.trending, name='trending'),
-    path('posts/pals/', views.palsposts, name='palsposts'),
-    path('posts/categories/', views.categories, name='categories'),
-    path('posts/categories/<slug:category_name_slug>/',views.show_category, name='show_category'),
-    path('account/signup/', views.signup, name='signup'),
-    path('account/login/', views.login, name='login'),
-    path('account/my_account/', views.my_account, name='my_account'),
-    path('account/my_account/make_post', views.make_post, name='make_post'),
-    path('account/my_account/edit_profile', views.edit_profile, name='edit_profile'),
-    path('account/my_account/add_pal', views.add_pal, name='add_pal'),
+    path('', views.index, name='index'),#amber
+    path('posts/trending/', views.trending, name='trending'),#nat
+    path('posts/pals/', views.palsposts, name='palsposts'),#nat
+    path('posts/categories/', views.categories, name='categories'),#nat
+    path('posts/categories/<slug:category_name_slug>/',views.show_category, name='show_category'),#nat
+    path('account/signup/', views.signup, name='signup'),#iryna
+    path('account/login/', views.login, name='login'),#iryna
+    path('account/my_account/', views.my_account, name='my_account'),#wang
+    path('account/my_account/make_post', views.make_post, name='make_post'),#amber
+    path('account/my_account/edit_profile', views.edit_profile, name='edit_profile'),#wang
+    path('account/my_account/add_pal', views.add_pal, name='add_pal'),#amber
 ]
