7aa01bba53bca224a78edce5a0c8898b8fec4483
2884069 <2884069w@student.gla.ac.uk>
2025-03-06 16:13:55 +0000

    models drafted

--- inqpal/models.py
+++ inqpal/models.py
@@ -1,33 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    fav_dino = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    def __str__(self):
+        return self.user.username
+
 class Post(models.Model):
     POST_MAX_LEN = 2000
 
-    # id?
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(Account, on_delete=models.CASCADE)
     text = models.CharField(max_length=POST_MAX_LEN)
     image = models.ImageField(upload_to='post_images', blank=True)
-    # category = 
-    # number_roars = models.IntegerField(default=0)
-    # roar relation (M:N)
+    category = models.CharField(max_length=50)
+    roars = models.ManyToManyField(Account)
 
     def __str__(self):
-        # should get some unique identifier
-        return self.id
+        return self.creator.__str__ + ":" + self.id
 
 class Comment(models.Model):
     COMMENT_MAX_LEN = 500
 
-    # id? if so based off Post id
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(Account, on_delete=models.CASCADE)
     text = models.CharField(max_length=COMMENT_MAX_LEN)
 
     def __str__(self):
-        # should get some unique identifier
-        return self.id
-    
-# class Account(models.Model):
-# implementation varies depending on how we decide on doing it
-# currently above assume utilising built-in django user
+        return self.post.__str__ + "-" + self.id
