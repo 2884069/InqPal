e96eab5a1be7814891ac548934e5241614ea51cd
“Iryna <2916265V@student.gla.ac.uk>
2025-03-07 11:00:11 +0000

    views signup

--- inqpal/views.py
+++ inqpal/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rango.models import Account
 
 
 def index(request):
@@ -17,7 +19,34 @@ def show_category(request):
     pass
 
 def signup(request):
-    pass
+    register = False
+
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        account_form = AccountForm(request.POST)
+
+        if user_form.is_valid() and account_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            account = account_form.save(commit=False)
+            account.user = user
+
+            if 'picture' in request.FILES:
+                account.picture = request.FILES['picture']
+            
+            account.save()
+            registred = True
+        
+        else:
+            print(user_form.errors, account_form.errors)
+        
+    else:
+        user_form = UserForm()
+        account_form = AccountForm()
+
+    return render(request, 'inqpal/register.html', context = {'user_form': user_form, 'account_form': account_form, 'registered': registred})
 
 def login(request):
     pass
